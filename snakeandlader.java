14707183c5418dd26afffeb533b0adde7bb2241d
rohitdighe19 <rohitdighe47@gmail.com>
2021-06-13 08:32:35 +0530

    solution of uc3

--- snakeandlader.java
+++ snakeandlader.java
@@ -2,10 +2,34 @@ import java.util.Random;
 
 public class snakeandlader {
 	
-	public static void main(String[] args)
-	{
-		Random randomObject = new Random();
-	int dice = randomObject.nextInt(6)+1;
-	System.out.println("Dice outcome is: "+dice);
-}
-}
+	
+		public static void main(String[] args)
+		{
+			int ladder = 1;
+			int snake = 2;
+			int noplay = 0;
+			int position = 0;
+
+			Random randomObject = new Random();
+			int dice = randomObject.nextInt(6)+1; //Returns random numbers between 1-6
+			System.out.println("Dice outcome is: "+dice);
+			int play = randomObject.nextInt(3); //Return random numbers between 0-2
+
+			if(play != noplay)
+			{
+				if(play==ladder)
+				{
+					position += dice;
+				}
+				else if(play == snake)
+				{
+					position -= dice;
+				}
+				System.out.println("New position gained is= "+position);
+			}
+			else
+			{
+				System.out.println("No play!");
+			}
+		}
+	}
