00484cb7d5399a76386664ae992ca472eee9d537
rohitdighe19 <rohitdighe47@gmail.com>
2021-06-13 08:38:15 +0530

    Added-solution to repeat the code till 100th position is reached

--- snakeandlader.java
+++ snakeandlader.java
@@ -3,33 +3,37 @@ import java.util.Random;
 public class snakeandlader {
 	
 	
-		public static void main(String[] args)
+	public static void main(String[] args)
+	{
+		int ladder = 1;
+		int snake = 2;
+		int noplay = 0;
+		int position = 0;
+
+		Random randomObject = new Random();
+
+		while(position<=100)
 		{
-			int ladder = 1;
-			int snake = 2;
-			int noplay = 0;
-			int position = 0;
-
-			Random randomObject = new Random();
 			int dice = randomObject.nextInt(6)+1; //Returns random numbers between 1-6
-			System.out.println("Dice outcome is: "+dice);
-			int play = randomObject.nextInt(3); //Return random numbers between 0-2
+        	        System.out.println("Dice outcome is: "+dice);
+	                int play = randomObject.nextInt(3); //Return random numbers between 0-2
 
-			if(play != noplay)
-			{
-				if(play==ladder)
+			if(play != noplay) {
+				if(play == ladder)
 				{
 					position += dice;
 				}
 				else if(play == snake)
 				{
 					position -= dice;
+					if(position<0)
+					{
+						position = 0;
+					}
 				}
 				System.out.println("New position gained is= "+position);
 			}
-			else
-			{
-				System.out.println("No play!");
-			}
 		}
+		System.out.println("Win!!.. The player has reached the 100th position.");
 	}
+}
