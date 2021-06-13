606a27f9f9c9a7879ee2ced8ef6a9bd8dd45953c
rohitdighe19 <rohitdighe47@gmail.com>
2021-06-13 08:51:09 +0530

    Added:solution for number of times the device was rolled

--- snakeandlader.java
+++ snakeandlader.java
@@ -1,14 +1,13 @@
 import java.util.Random;
 
 public class snakeandlader {
-	
-	
 	public static void main(String[] args)
 	{
 		int ladder = 1;
 		int snake = 2;
 		int noplay = 0;
 		int position = 0;
+		int diceRoll = 0;
 
 		Random randomObject = new Random();
 
@@ -17,6 +16,8 @@ public class snakeandlader {
 			int dice = randomObject.nextInt(6)+1; //Returns random numbers between 1-6
         	        System.out.println("Dice outcome is: "+dice);
 	                int play = randomObject.nextInt(3); //Return random numbers between 0-2
+
+			diceRoll++;
 
 			if(play != noplay) {
 				if(play == ladder)
@@ -38,6 +39,7 @@ public class snakeandlader {
 				System.out.println("New position gained is= "+position);
 			}
 		}
-		System.out.println("Win!!.. The player has reached the exact 100th position.");
+		System.out.println("Win!!.. The player has reached the exact 100th position.\n");
+		System.out.println("The dice was rolled "+diceRoll+" number of times.");
 	}
 }
