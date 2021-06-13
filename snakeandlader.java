8f437e2a86622c49468e7cf141e5abe42be0bd7f
rohitdighe19 <rohitdighe47@gmail.com>
2021-06-13 08:06:21 +0530

    initial commit

---
+++ snakeandlader.java
@@ -0,0 +1,10 @@
+
+public class snakeandlader {
+	public static void main(String[] args)
+	{
+		int position = 0;
+		System.out.println("Welcome to snake & ladder game, we have a single player at position "+position);
+	}
+}
+
+
